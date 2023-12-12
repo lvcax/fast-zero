c09038c03e04e4551765f71b27c70d5e0d522d38
Lucas Ferreira <lucasferreiraek@gmail.com>
2023-12-12 12:31:45 -0300

    adicionada a primeira migração com alembic. criada tabela de usuários

---
+++ tests/test_db.py
@@ -0,0 +1,13 @@
+from sqlalchemy import select
+
+from fast_zero.models import User
+
+
+def test_create_user(session):
+    new_user = User(username='alice', password='senhatop', email='alice@example.com')
+    session.add(new_user)
+    session.commit()
+
+    user = session.scalar(select(User).where(User.username == 'alice'))
+
+    assert user.username == 'alice'
