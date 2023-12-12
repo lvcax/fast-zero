e8bec7bf4b195d54b19dc0c75c12c57fd8c0a23e
Lucas Ferreira <lucasferreiraek@gmail.com>
2023-12-12 09:48:02 -0300

    implementando rotas crud

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
+from fast_zero.schemas import UserSchema, UserPublic, UserDB, UserList, Message
 
 app = FastAPI()
+database = []
 
 
 @app.get('/')
 def read_root():
     return {'message': 'ola mundo'}
+
+
+@app.post('/users/', status_code=201, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=404, detail='user not found')
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=404, detail='user not found')
+
+    del database[user_id - 1]
+
+    return {'detail': 'user deleted'}
