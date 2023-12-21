c90ae4dbffce6482ba39ba19b96e3b970af674d9
Lucas Ferreira <lucasferreiraek@gmail.com>
2023-12-21 02:32:17 -0300

    Implement DELETE and PATCH endpoints for todos

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,9 +1,10 @@
 from sqlalchemy import select
+from sqlalchemy.orm import Session
 
-from fast_zero.models import User
+from fast_zero.models import Todo, User
 
 
-def test_create_user(session):
+def test_create_user_without_todos(session):
     new_user = User(
         username='alice', password='senhatop', email='alice@example.com'
     )
@@ -13,3 +14,19 @@ def test_create_user(session):
     user = session.scalar(select(User).where(User.username == 'alice'))
 
     assert user.username == 'alice'
+
+def test_create_todo(session: Session, user: User):
+    todo = Todo(
+        title='Test todo',
+        description='Test desc',
+        state='draft',
+        user_id=user.id
+    )
+
+    session.add(todo)
+    session.commit()
+    session.refresh(todo)
+
+    user = session.scalar(select(User).where(User.id == user.id))
+
+    assert todo in user.todos
